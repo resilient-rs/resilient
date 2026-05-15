0f78d2c9db38dd86e9af47135504cac1da7babca
kaleab yemane <kaleaby49@gmail.com>
2026-05-15 10:41:22 +0300

    feat: Add RetryBuilder with Exponential strategy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
-// Library logic goes here...
+use std::time::Duration;
 
-pub fn foo() {}
+pub struct Fixed;
+
+pub struct Exponential;
+
+pub struct Linear;
+
+pub struct RetryBuilder<S> {
+    pub max_attempts: u32,
+    pub base_delay: Duration,
+    pub max_delay: Duration,
+    pub jitter: Option<f64>,
+    _strategy: std::marker::PhantomData<S>,
+}
+
+impl RetryBuilder<Exponential> {
+    pub fn exponential(max_attempts: u32) -> RetryBuilder<Exponential> {
+        RetryBuilder {
+            max_attempts,
+            base_delay: Duration::from_millis(100),
+            max_delay: Duration::from_secs(30),
+            jitter: None,
+            _strategy: std::marker::PhantomData,
+        }
+    }
+}
