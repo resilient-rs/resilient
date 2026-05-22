2075491744dc8882e10f8ce59221d92b180d29cc
yofabr <jofab08@gmail.com>
2026-05-22 21:41:36 +0300

    chore: Flake nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "resilient - Composable async library for rust";
+
+  nixConfig = {
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      overlays = [ rust-overlay.overlays.default ];
+      pkgs = import nixpkgs { inherit overlays; system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+          gcc
+          pkg-config
+        ];
+      };
+    };
+}
